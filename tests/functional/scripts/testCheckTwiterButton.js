223e63488606a7d5b8200492746739337b4736ce
Crisleicon Francisco <ctf@br.ibm.com>
2017-09-26 14:26:35 +0000

    changes4

--- tests/functional/scripts/testCheckTwiterButton.js
+++ tests/functional/scripts/testCheckTwiterButton.js
@@ -2,9 +2,8 @@ module.exports = {
   "Demo test RangoBot" : function (browser) {
     browser
       .url("http://rangobot.mybluemix.net")
-      .waitForElementVisible('body', 1000)
-      .pause(1000)
-     .click('button[name=Twitter]')
+      .waitForElementVisible('#id_twitter', 1000)
+     .click('#id_twitter')
       .end();
   }
 };
